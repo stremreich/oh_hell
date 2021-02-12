a275014322c408c47493cd64050fd5b0553a863a
Stephen Trembath-Reichert <stephen.trembath@gmail.com>
2021-02-12 18:54:13 -0500

    implemented Game and Player classes

--- oh_hell.py
+++ oh_hell.py
@@ -1,9 +1,7 @@
 import random
 
 suit_names = ["Spades", "Clubs", "Hearts", "Diamonds"]
-card_vals = ["Two", "Three", "Four", "Five", \
-			"Six", "Seven", "Eight", "Nine", "Ten", \
-			"Jack", "Queen", "King", "Ace"]
+card_vals = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"]
 
 class Card:
 	suit = 0
@@ -45,16 +43,69 @@ class Deck:
 			print(line)
 		print("----")
 
+class Player:
+	name = ""
+	hand = []
+	score = 0
+
+	def __init__(self):
+		self.name = "Player"
+		self.score = 0
+
+	def __init__(self, name):
+		self.name = name
+		self.score = 0
+
+	def printPlayerInfo(self):
+		print('Info for player %s' % self.name)
+		print('Score: %d' % self.score)
+		print('Hand contents:')
+		for card in self.hand:
+			print(card.getName())
+
+
+
+class Game:
+	players = []
+	hook = True
+	maxHands = 7
+	deck = Deck()
+
+	def __init__(self, pNames, nHands, hookBool):
+		self.maxHands = nHands
+		self.hook = hookBool
+		self.deck = Deck()
+		for name in pNames:
+			self.players.append(Player(name))
+
+	def printGameInfo(self):
+		print('Game Info: ')
+		#print('Number of players: %d' % len(self.playerNames))
+		#print('Players:')
+		#for pName in self.playerNames:
+		#  	print(pName)
+		print('Maximum starting cards: %d' % self.maxHands)
+		#print('Maximum possible starting cards: %d' % int(52 / len(self.playerNames)))
+		print('Player Scores: ')
+		for player in self.players:
+			print('%s: %d' % (player.name, player.score))
+		print('Deck contents: ')
+		self.deck.printOrderSh()
+		
+	def play(self):
+		#TODO: implement oh hell! game rules
+
 
 c = Card(8,1)
 c2 = Card(11,3)
 d = Deck()
+p = Player("testPlayer")
+p.hand = [c, c2]
 
-print(c2.suit)
-print(c2.val)
-print(c2.getName())
-print(c2.getNameSh())
+p.printPlayerInfo()
 
-d.printOrderSh()
-d.shuffle()
-d.printOrderSh()
+pN = ['STR', 'ETR', 'Mom', '3P', 'PJ', 'CJ']
+
+g = Game(pN, 7, True)
+g.printGameInfo()
+
